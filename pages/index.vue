6aff437ae25da8128ceba6dc7441cd66c9e3576d
Vanege <11367082+Vanege@users.noreply.github.com>
2019-12-16 22:58:19 +0100

    usa klasi komponentu

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     Startu!
+    {{ text }}
+    <v-btn @click="changeText" />
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  components: {}
+import { Component } from 'vue-property-decorator'
+import Vue from 'vue'
+
+@Component
+export default class App extends Vue {
+  text: string | null = null;
+
+  changeText() {
+    this.text = 'aaaa'
+  }
 }
 </script>
