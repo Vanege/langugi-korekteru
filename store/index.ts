95284ca5df03d5a92acfb24c56761a9b7f2e95be
Vanege <11367082+Vanege@users.noreply.github.com>
2019-12-21 11:21:26 +0100

    geta wordu fromo Google

--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,4 @@
-import { isString, isArray } from "~/tools/type-guards"
+import { isString, isArray, isDefined } from "~/tools/type-guards"
 import { Store } from "vuex/types/index"
 
 type State = {
@@ -10,7 +10,7 @@ export const state = (): State => ({
 })
 
 export const mutations = {
-  setLanguguWords (state: State, payload: any) {
+  setLanguguWords (state: State, payload) {
     if (isArray(payload) && payload.every(isString)) {
       state.languguWords = payload
     }
@@ -19,7 +19,16 @@ export const mutations = {
 
 export const actions = {
   async getLanguguWords (store: Store<State>) {
-    
-    store.commit('setLanguguWords', ['aaa', 'bbb'])
+    const googleSpreadsheetId = "1guKRbFVMjat0CFsaqEdqzTiLMMgT4dIfyAlbmf2J7wI"
+    const url = `https://spreadsheets.google.com/feeds/cells/${googleSpreadsheetId}/1/public/full?alt=json`
+    const json = await (this as any).$axios.$get(url)
+    const entries = json.feed.entry
+    const languguEntries = entries.filter(isEntryInLangugu)
+    const languguWords = languguEntries.map(lE => lE.content.$t)
+    store.commit('setLanguguWords', languguWords)
   }
+}
+
+const isEntryInLangugu = (entry): boolean => {
+  return isDefined(entry) && isDefined(entry.gs$cell) && entry.gs$cell.col === "3"
 }
