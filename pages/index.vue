eb095bcd8f96ec24474f916ae7d88189f67523d6
Vanege <11367082+Vanege@users.noreply.github.com>
2019-12-21 20:53:49 +0100

    ada tekstplasu foro verifa

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    Startu!
-    {{ languguWords }}
-    <v-btn @click="getLanguguWords" />
+    <textarea v-model="text" type="text" rows="4" cols="80" placeholder="Type your text here (ex: mu wanta huga yu)" />
+    <hr>
+    <div v-html="checkedText" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import { Context } from '@nuxt/types'
+import { isNull } from '~/tools/type-guards'
 
 @Component
 export default class App extends Vue {
@@ -16,8 +17,18 @@ export default class App extends Vue {
 
   get languguWords(): string[] | null { return this.$store.state.languguWords }
 
-  getLanguguWords() {
-    this.$store.dispatch('getLanguguWords')
+  get checkedText(): string {
+    if (isNull(this.text) || isNull(this.languguWords)) { return '' }
+    const parsedWords = this.text.split(' ')
+    const languguWords = this.languguWords
+    const checkedWords = parsedWords.map((parsedWord) => {
+      if (languguWords.includes(parsedWord)) {
+        return parsedWord
+      } else {
+        return `<font color="red">${parsedWord}</font>`
+      }
+    })
+    return checkedWords.join(' ')
   }
 
   async fetch({ store }: Context) {
--- tools/type-guards.ts
+++ tools/type-guards.ts
@@ -1,4 +1,4 @@
-export function isString(value) {
+export function isString(value): value is string {
   return typeof value === 'string' || value instanceof String
 }
 
@@ -6,23 +6,23 @@ export function isArray(value) {
   return Array.isArray(value)
 }
 
-export function isNumber(value) {
+export function isNumber(value): value is number {
   return typeof value === 'number'
 }
 
-export function inNaN(value) {
+export function isNaN(value) {
   return Number.isNaN(value)
 }
 
-export function isBoolean(value) {
+export function isBoolean(value): value is boolean {
   return typeof value === 'boolean'
 }
 
-export function isNull(value) {
+export function isNull(value): value is null {
   return value === null
 }
 
-export function isUndefined(value) {
+export function isUndefined(value): value is undefined {
   return typeof value === 'undefined'
 }
 
