83e882ae2687cd2b4d2b811689ce2ad091f5b5ad
Vanege <11367082+Vanege@users.noreply.github.com>
2019-12-22 18:25:46 +0100

    parsa tekstu

---
+++ components/Element.vue
@@ -0,0 +1,13 @@
+<template>
+  <span>{{ element.string }}</span>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import { ParsedElement } from '~/pages/index.vue'
+
+@Component
+export default class Element extends Vue {
+  @Prop() readonly element!: ParsedElement
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,33 +2,55 @@
   <div>
     <textarea v-model="text" type="text" rows="4" cols="80" placeholder="Type your text here (ex: mu wanta huga yu)" />
     <hr>
-    <div v-html="checkedText" />
+    <Element v-for="(element, index) in parsedElements" :key="index" :element="element" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import { Context } from '@nuxt/types'
-import { isNull } from '~/tools/type-guards'
+import Element from '~/components/Element.vue'
 
-@Component
+// the whole text to check is divided into elements that can be words (any string of allowed characters), or an other character
+export type ParsedElement = {
+  // a parsed element is a word if every character of the original string is a word character
+  isAWord: boolean;
+  string: string;
+}
+
+@Component({
+  components: {
+    Element
+  }
+})
 export default class App extends Vue {
-  text: string | null = null;
+  text: string = ''
 
   get languguWords(): string[] | null { return this.$store.state.languguWords }
 
-  get checkedText(): string {
-    if (isNull(this.text) || isNull(this.languguWords)) { return '' }
-    const parsedWords = this.text.split(' ')
-    const languguWords = this.languguWords
-    const checkedWords = parsedWords.map((parsedWord) => {
-      if (languguWords.includes(parsedWord)) {
-        return parsedWord
+  get parsedElements(): ParsedElement[] {
+    const parsedElements: ParsedElement[] = []
+    let wordInConstruction: string = ''
+    for (const char of this.text) {
+      if (/[a-zA-Z]/.test(char)) {
+        wordInConstruction += char
       } else {
-        return `<font color="red">${parsedWord}</font>`
+        if (wordInConstruction.length > 0) {
+          const newWordElement: ParsedElement = {
+            isAWord: true,
+            string: wordInConstruction
+          }
+          parsedElements.push(newWordElement)
+        }
+        const newElement: ParsedElement = {
+          isAWord: false,
+          string: char
+        }
+        parsedElements.push(newElement)
+        wordInConstruction = ''
       }
-    })
-    return checkedWords.join(' ')
+    }
+    return parsedElements
   }
 
   async fetch({ store }: Context) {
