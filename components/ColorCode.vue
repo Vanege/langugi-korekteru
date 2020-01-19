4338989442716b94d7a00a13549ff1c8167119a5
Vanege <11367082+Vanege@users.noreply.github.com>
2020-01-19 10:12:23 +0100

    "+ maybi trapu" -> "buto ofteni kana ba eroru"

--- components/ColorCode.vue
+++ components/ColorCode.vue
@@ -1,9 +1,9 @@
 <template>
   <v-sheet class="yellow lighten-5">
     <div><span class="green round-borders">greni</span>: bo wordlistu</div>
-    <div><span class="yellow round-borders bold">yelowi</span>: bo wordlistu + maybi trapu</div>
+    <div><span class="yellow round-borders bold">yelowi</span>: bo wordlistu buto ofteni kana ba eroru</div>
     <div><span class="orange round-borders italic">orangi</span>: no bo wordlistu</div>
-    <div><span class="red round-borders italic bold">redi</span>: no bo wordlistu + maybi trapu</div>
+    <div><span class="red round-borders italic bold">redi</span>: no bo wordlistu buto ofteni kana ba eroru</div>
   </v-sheet>
 </template>
 
